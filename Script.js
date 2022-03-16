6e006a50ac4eeb7c70993ebd6e8d873b47f1f1b0
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2022-03-16 17:37:16 -0400

    Added a bunch of stuff to script.js

--- Script.js
+++ Script.js
@@ -38,3 +38,42 @@ function HTMLB(ClassName, ButtonID) {
 function backgroundColor(color) {
 document.body.style.background = color;
 }
+class block {
+	size = 16
+	mss = 64
+	constructor(name, ss, blockt, blockmd) {
+		this.name = name;
+		this.ss = ss;
+		this.blockt = blockt;
+		this.blockm = blockmd;
+	}
+}
+let block1 = new block("Block #1", 16, "FFF", true)
+let block2 = new block("Block #2", 32, "00F", false)
+let block3 = new block("Block #3", 64, "0F0", false)
+let block4 = new block("Block #4", 56, "F0F", true)
+let block5 = new block("Block #5", 64, "000", true)
+let block6 = new block("Block #6", 16, "FF0", false)
+let block7 = new block("Block #7", 16, "0FF", false)
+let block8 = new block("Block #8", 64, "F00", true)
+class box {
+	defaultsize = "2x2"
+	constructor(label, contained, shape, to, from) {
+		this.name = label
+		this.contents = contained;
+		this.shape = shape;
+		this.receiver = to;
+		this.sender = to;
+	}
+}
+class blockbox extends box {
+	constructor(name, blocks, shape) {
+		super(shape);
+		this.name = name;
+		this.blocks = blocks;
+	}
+}
+let blockbox1 = new blockbox("Blockbox #1", [block1, block2, block3, block4, block5, block6, block7, block8], box.dsize)
+logString(blockbox1.name)
+logString(blockbox1.shape)
+logString(blockbox1.blocks[0].name)
